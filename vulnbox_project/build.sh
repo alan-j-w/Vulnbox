8b9fea815b6b30e3e205cab67c2524881263fdb8
alan-j-w <alanjoywilson@gmail.com>
2026-04-03 02:45:44 +0530

    Deployment: Final sync with Render Root Directory setting

--- vulnbox_project/build.sh
+++ vulnbox_project/build.sh
@@ -2,6 +2,7 @@
 # Exit on error
 set -o errexit
 
+# Since Root Directory is set to vulnbox_project, we are already in the right place!
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
