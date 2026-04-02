1e3911dc6cd61cf911465ba62dbf3051c1916922
alan-j-w <alanjoywilson@gmail.com>
2026-04-03 02:28:07 +0530

    Fix: Final Allauth settings compliance

--- vulnbox_project/vulnbox/settings.py
+++ vulnbox_project/vulnbox/settings.py
@@ -203,10 +203,8 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 # Allauth settings (Modern Format)
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_USERNAME_REQUIRED = True
 ACCOUNT_LOGIN_METHODS = {'email'}
-ACCOUNT_SIGNUP_FIELDS = ['email', 'username', 'password']
+ACCOUNT_SIGNUP_FIELDS = ['email', 'username', 'password1']
 ACCOUNT_EMAIL_VERIFICATION = 'none' # Set to 'mandatory' if you add email backend
 SOCIALACCOUNT_AUTO_SIGNUP = True
 SOCIALACCOUNT_ADAPTER = 'authapp.adapters.CustomSocialAccountAdapter'
